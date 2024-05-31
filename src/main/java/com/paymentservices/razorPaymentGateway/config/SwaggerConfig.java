05e15489d886bcf14dc9d8e1c850891b83af135a
Divya Gupta <divygupta0319@gmail.com>
2024-05-31 22:00:13 +0530

    Removed WebConfigurer interface extending line

--- src/main/java/com/paymentservices/razorPaymentGateway/config/SwaggerConfig.java
+++ src/main/java/com/paymentservices/razorPaymentGateway/config/SwaggerConfig.java
@@ -3,7 +3,6 @@ package com.paymentservices.razorPaymentGateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,7 +13,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 @EnableWebMvc
-public class SwaggerConfig implements WebMvcConfigurer {
+public class SwaggerConfig {
 
     @Bean
     public Docket api() {
