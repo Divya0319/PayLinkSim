c22d4639325e2c00324cf0a5a6d4ad338e073234
Divya Gupta <divygupta0319@gmail.com>
2024-05-31 21:14:42 +0530

    Integrated SpringDoc-OpenAPI successfully, and tested successfully as well

--- src/main/java/com/paymentservices/razorPaymentGateway/config/SwaggerConfig.java
+++ src/main/java/com/paymentservices/razorPaymentGateway/config/SwaggerConfig.java
@@ -1,38 +1,36 @@
 package com.paymentservices.razorPaymentGateway.config;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @Configuration
-@EnableWebMvc
-public class SwaggerConfig implements WebMvcConfigurer {
+public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.paymentservices.razorPaymentGateway.controllers"))
-                .paths(PathSelectors.regex("/.*"))
-                .build().apiInfo(apiInfoMetaData());
+    public GroupedOpenApi api() {
+    	return GroupedOpenApi.builder()
+    			.group("razorPay-gateway")
+    			.packagesToScan("com.paymentservices.razorPaymentGateway.controllers")
+    			.build();
     }
 
-    private ApiInfo apiInfoMetaData() {
-
-        return new ApiInfoBuilder().title("RazerPay Payment Gateway")
-                .description("For API learning")
-                .contact(new Contact("Divya Gupta", "https://github.com/Divya0319/", "ddivygupta0319@gmail.com"))
-                .license("Apache 2.0")
-                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-                .version("1.0.0")
-                .build();
+    @Bean
+    public OpenAPI orderProcessOpenAPI() {
+    	return new OpenAPI()
+    			.info(new Info().title("RazorPay Payment Gateway")
+				.description("For API Learning")
+				.version("1.0.0")
+				.license(new License().name("Apache 2.0").url("http://springdoc.org")))
+				.externalDocs(new ExternalDocumentation()
+				.description("SpringShop Wiki Documentation")
+	            .url("https://springshop.wiki.github.org/docs"));
     }
 
 }
