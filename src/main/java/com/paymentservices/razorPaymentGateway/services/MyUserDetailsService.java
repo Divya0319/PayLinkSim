061f863fac3174616cf86a928d51b25036176348
Divya Gupta <divygupta0319@gmail.com>
2024-06-08 21:40:16 +0530

    Added UserDetailService to load user-specific data

---
+++ src/main/java/com/paymentservices/razorPaymentGateway/services/MyUserDetailsService.java
@@ -0,0 +1,19 @@
+package com.paymentservices.razorPaymentGateway.services;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyUserDetailsService implements UserDetailsService{
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// For demo, returning a user with username and password
+        // In real applications, you should fetch user details from a database
+        return new User("user", "password", new ArrayList<>());
+	}
+
+}
