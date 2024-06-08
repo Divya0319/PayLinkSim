8febe9172e67fde490a3a7105c620614a616fda6
Divya Gupta <divygupta0319@gmail.com>
2024-06-08 11:36:41 +0530

    Started integration of Authentication for APIs using JWT authentication

--- src/main/java/com/paymentservices/razorPaymentGateway/config/SwaggerConfig.java
+++ src/main/java/com/paymentservices/razorPaymentGateway/config/SwaggerConfig.java
@@ -5,10 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
@@ -24,6 +27,8 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI orderProcessOpenAPI() {
     	return new OpenAPI()
+    			.addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+    			.components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
     			.info(new Info().title("RazorPay Payment Gateway")
 				.description("For API Learning")
 				.version("1.0.0")
@@ -31,6 +36,12 @@ public class SwaggerConfig {
 				.externalDocs(new ExternalDocumentation()
 				.description("SpringShop Wiki Documentation")
 	            .url("https://springshop.wiki.github.org/docs"));
+    }
+    
+    private SecurityScheme createAPIKeyScheme() {
+    	return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+    			.bearerFormat("JWT")
+    			.scheme("bearer");
     }
 
 }
--- src/main/java/com/paymentservices/razorPaymentGateway/controllers/PaymentController.java
+++ src/main/java/com/paymentservices/razorPaymentGateway/controllers/PaymentController.java
@@ -10,6 +10,8 @@ import com.paymentservices.razorPaymentGateway.models.PaymentStatus;
 import com.paymentservices.razorPaymentGateway.models.PaymentSuccess;
 import com.paymentservices.razorPaymentGateway.services.PaymentService;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 public class PaymentController {
 	
@@ -20,16 +22,19 @@ public class PaymentController {
 		this.paymentService = paymentService;
 	}
 	
+	@Operation(summary = "It creates the payment link, which we can copy paste in browser.")
 	@PostMapping("/payment/createLink")
 	public String createPaymentLink(@RequestParam String orderId, @RequestParam int amount) {
 		return paymentService.createLink(orderId, amount);
 	}
 	
+	@Operation(summary = "It returns whether a payment was success or failure")
 	@GetMapping("/payment/getPaymentStatus")
 	public PaymentStatus getPaymentStatus(@RequestParam("paymentId") String paymentId, @RequestParam("orderId") String orderId) {
 		return paymentService.getPaymentStatus(paymentId, orderId);
 	}
 	
+	@Operation(summary = "Used for internal callbacks, not to be used by user")
 	@GetMapping("/payment/paymentCallback")
     public PaymentSuccess handleRazorpayCallback(
             @RequestParam("razorpay_payment_id") String paymentId,
