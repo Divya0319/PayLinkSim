0d1d026abc496c713fd941f8548695b311be9aa5
Divya Gupta <divygupta0319@gmail.com>
2024-06-10 13:05:37 +0530

    Forgot to add auth/login and payment/paymentCallback urls in filter class, now everything works fine, and header is null issue is gone now

--- src/main/java/com/paymentservices/razorPaymentGateway/filter/JwtAuthFilter.java
+++ src/main/java/com/paymentservices/razorPaymentGateway/filter/JwtAuthFilter.java
@@ -2,7 +2,10 @@ package com.paymentservices.razorPaymentGateway.filter;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,63 +36,108 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     private UserDetailsService userDetailsService;
 	
 	private Logger logger = LoggerFactory.getLogger(OncePerRequestFilter.class);
+	
+	private static final List<String> EXCLUDED_URLS = Arrays.asList(
+			"/auth/login",					// Exclude login endpoint
+			"/payment/paymentCallback",		// Exclude callback endpoint
+			"/platformpayment/swagger-ui/",  // Exclude Swagger UI 
+            "/v2/api-docs/",
+            "/v3/api-docs/",
+            "/swagger-resources/",
+            "/webjars/",
+            "/swagger-ui/"
+    );
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		
+		String requestUri = request.getRequestURI();
+		
+		if(isExcluded(requestUri)) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		
 		String requestHeader = request.getHeader("Authorization");
 		logger.info("Header : {}", requestHeader);
 		String username = null;
 		String token = null;
-		if(requestHeader != null && requestHeader.startsWith("Bearer")) {
-			// looking good
-			token = requestHeader.substring(7);
-			
-			try {
-				username = this.jwtHelper.getUsernameFromToken(token);
-			} catch(IllegalArgumentException e) {
-				logger.info("Illegal Argument while fetching the username !!");
-				PrintWriter writer = response.getWriter();
-				writer.println("Illegal Argument while fetching the username !!");
-            } catch (ExpiredJwtException e) {
-                logger.info("Given jwt token is expired !!");
-                PrintWriter writer = response.getWriter();
-    			writer.println("Given jwt token is expired !!");
-            } catch (MalformedJwtException e) {
-                logger.info("Some changes has done in token !! Invalid Token");
-                PrintWriter writer = response.getWriter();
-    			writer.println("Some changes has done in token !! Invalid Token");
-            } catch (Exception e) {
-            	logger.info("Access Denied !! {}", e.getMessage());
-            	PrintWriter writer = response.getWriter();
-    			writer.println("Access Denied !! "  + e.getMessage());
-
-            }
-		} else {
-			logger.info("Invalid Header value!! ");
-		}
 		
-		if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-			 //fetch user detail from username
-			UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
-			Boolean validateToken = this.jwtHelper.validateToken(token, userDetails);
-			
-			if(validateToken) {
-				
-				// set the authentication
-				UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-				authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-				SecurityContextHolder.getContext().setAuthentication(authentication);
-			} else {
-				logger.info("Validation fails !!");
+		if(requestHeader == null) {
+            logger.info("Header is null !!");
+            handleError(response, "Header is null !!");
+		} 
+		else {
+			if(StringUtils.isBlank(requestHeader)) {
+				logger.info("Header is blank !!");
+				handleError(response, "Header is blank !!");
+			} 
+			else {
+				if (requestHeader.startsWith("Bearer ")) {
+		            // Extract token after "Bearer " prefix
+		            token = requestHeader.substring(7);
+		            
+		            try {
+		                username = this.jwtHelper.getUsernameFromToken(token);
+		            } catch (IllegalArgumentException e) {
+		                logger.info("Illegal Argument while fetching the username !!");
+		                handleError(response, "Illegal Argument while fetching the username !!");
+		                return;
+		            } catch (ExpiredJwtException e) {
+		                logger.info("Given jwt token is expired !!");
+		                handleError(response, "Given jwt token is expired !!");
+		                return;
+		            } catch (MalformedJwtException e) {
+		                logger.info("Some changes have been made to the token !! Invalid Token");
+		                handleError(response, "Some changes have been made to the token !! Invalid Token");
+		                return;
+		            } catch (Exception e) {
+		                logger.info("Access Denied !! {}", e.getMessage());
+		                handleError(response, "Access Denied !! " + e.getMessage());
+		                return;
+		            }
+		        } else {
+		        	logger.info("Header is blank !!");
+					handleError(response, "Header is blank !!");
+		        }
 			}
-			
-			
 		}
+        
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // Fetch user details from username
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+            Boolean validateToken = this.jwtHelper.validateToken(token, userDetails);
+            
+            if (validateToken) {
+                // Set the authentication
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                logger.info("Validation failed !!");
+                handleError(response, "Validation failed !!");
+                return;
+            }
+        }
 		
-		filterChain.doFilter(request, response);
+        if(!response.isCommitted()) {
+        	filterChain.doFilter(request, response); 
+        }
 		
 	}
+	
+	private boolean isExcluded(String requestUri) {
+		return EXCLUDED_URLS.stream().anyMatch(excludedUrl -> requestUri.startsWith(excludedUrl));
+	}
+	
+	private void handleError(HttpServletResponse response, String message) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        PrintWriter writer = response.getWriter();
+        writer.write("{\"error\": \"" + message + "\"}");
+        writer.flush();
+        writer.close();
+    }
 
 }
