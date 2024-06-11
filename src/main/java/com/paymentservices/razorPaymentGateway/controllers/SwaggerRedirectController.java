583d2503b7a2c1b7ba5fe6d5237441472a4d1435
Divya Gupta <divygupta0319@gmail.com>
2024-06-11 12:06:38 +0530

    Added redirect to swagger UI url when index.html is not given in url

---
+++ src/main/java/com/paymentservices/razorPaymentGateway/controllers/SwaggerRedirectController.java
@@ -0,0 +1,15 @@
+package com.paymentservices.razorPaymentGateway.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class SwaggerRedirectController {
+
+	@GetMapping("/platformpayment/swagger-ui/")
+	public RedirectView redirectToSwaggerUI() {
+		return new RedirectView("/platformpayment/swagger-ui/index.html#");
+	}
+	
+}
